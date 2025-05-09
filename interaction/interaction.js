5fb24cc047e5f01d8e40d40328574495f7f750ae
iamdaniel14 <96418140+iamdaniel14@users.noreply.github.com>
2025-05-09 16:41:37 +0200

    second commit

--- interaction/interaction.js
+++ interaction/interaction.js
@@ -1,21 +1,26 @@
-const HAMBURGER_ICON=document.querySelector (".nav_container__menu-icon");
+const HAMBURGER_ICON=document.querySelector (".nav_container__menu_icon");
 let hamburger_icon_is_clicked=false;
 
 HAMBURGER_ICON.addEventListener ("click", () =>{
 const NAV_LIST_CONTAINER=document.querySelector (".nav_container__list");
-
-
-
 if (!hamburger_icon_is_clicked) {
     hamburger_icon_is_clicked=true;
-    NAV_LIST_CONTAINER.classList.remove ("nav_container__list-remove");
-    NAV_LIST_CONTAINER.classList.add ("nav_container__list-show");
-    console.log ("clicked")
+    NAV_LIST_CONTAINER.classList.remove ("nav_container__list_remove");
+    NAV_LIST_CONTAINER.classList.add ("nav_container__list_show");
+        document.body.classList.add('no-scroll'); 
+
 
 } else if (hamburger_icon_is_clicked){
     hamburger_icon_is_clicked=false;
-    NAV_LIST_CONTAINER.classList.remove ("nav_container__list-show");
-    NAV_LIST_CONTAINER.classList.add ("nav_container__list-remove");
+    NAV_LIST_CONTAINER.classList.remove ("nav_container__list_show");
+    NAV_LIST_CONTAINER.classList.add ("nav_container__list_remove");
+        document.body.classList.remove('no-scroll'); 
+
+
 }
 
 })
+
+
+
+
