59426725afaba2949274bf91603a83a91abc5fc5
iamdaniel14 <96418140+iamdaniel14@users.noreply.github.com>
2025-05-10 16:39:45 +0200

    fixing the layout

--- interaction/interaction.js
+++ interaction/interaction.js
@@ -7,19 +7,38 @@ if (!hamburger_icon_is_clicked) {
     hamburger_icon_is_clicked=true;
     NAV_LIST_CONTAINER.classList.remove ("nav_container__list_remove");
     NAV_LIST_CONTAINER.classList.add ("nav_container__list_show");
-        document.body.classList.add('no-scroll'); 
 
 
-} else if (hamburger_icon_is_clicked){
+}  else {
     hamburger_icon_is_clicked=false;
-    NAV_LIST_CONTAINER.classList.remove ("nav_container__list_show");
     NAV_LIST_CONTAINER.classList.add ("nav_container__list_remove");
-        document.body.classList.remove('no-scroll'); 
+    NAV_LIST_CONTAINER.classList.remove ("nav_container__list_show");
+
+}
+
+}
+
+)
+
+const exist_ICON=document.querySelector (".nav_container__exist");
+let exist_icon_is_clicked=true;
+
+exist_ICON.addEventListener ("click", () =>{
+const NAV_LIST_CONTAINER=document.querySelector (".nav_container__list");
+if (exist_icon_is_clicked) {
+    hamburger_icon_is_clicked=false;
+     NAV_LIST_CONTAINER.classList.add ("nav_container__list_remove");
+     NAV_LIST_CONTAINER.classList.remove ("nav_container__list_show");
 
 
 }
 
 })
+
+
+
+
+
 
 
 
